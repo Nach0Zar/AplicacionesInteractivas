b815e769074b4c17d113d53367cd4dba2d4dfbf8
Nach0Zar <ignaciozarlenga@hotmail.com>
2023-10-01 20:14:37 -0300

    Fixed FAQs and About us

--- client/src/components/faqs/Faqs.jsx
+++ client/src/components/faqs/Faqs.jsx
@@ -7,15 +7,19 @@ const Faqs = () => {
       <div className="faqsContainer">
         <div className="preguntaDiv">
             <h3>¿Cómo puedo comprar?</h3>
-            <p>Para comprar productos, es necesario primero registrarse en la página o, en caso de ya contar con una cuenta, loguearse. Una vez dentro de la cuenta, iremos al buscador a elegir el elemento que deseamos adquirir. Al comprar el elemento, deberemos elegir método de pago y domicilio del comprador.</p>    
+            <p>Para adquirir servicios, es necesario contar con un email y un telefono de contacto. Una vez enviada la solicitud, cada proveedor de servicio podra analizar la peticion y se pondra en contacto via email/telefono para coordinar los pasos a seguir.</p>    
         </div>
         <div className="preguntaDiv">
-            <h3>¿Puedo comprar items de otro pais?</h3>
-            <p>En Zona Compra contamos con una licencia en los paises miembro del MERCOSUR para el libre traspaso de mercadería. Una compra dentro del mismo pais en el que se encuentra el vendedor y el comprador tiene un aproximado de 15 días hábiles. Para compras entre diferentes paises, el estimado de envío está entre 20 a 30 días hábiles desde que se efectua la compra.</p>    
+            <h3>¿Puedo comprar servicios de otro pais?</h3>
+            <p>En Cursera contamos con una licencia en los paises miembro del MERCOSUR para el libre comercio de servicios.</p>    
         </div>
         <div className="preguntaDiv">
-            <h3>No me llegó un item o quiero cancelar la compra, ¿Con quien puedo hablar?</h3>
-            <p>Zona Compra cuenta con múltiples centros de soporte y ayuda al consumidor vía telefónica. Además, cuenta con un chat interactivo desde la página para realizar cancelaciones previas a las 24horas de hecha la compra sin ningún tipo de costo. Pasadas esas 24 horas, se deberá hablar con el soporte para coordinar las acciones a tomar y se devolverá la totalidad o una parcialidad de la compra. </p>    
+            <h3>Quiero proveer mis propios servicios, ¿Como puedo hacerlo?</h3>
+            <p>Cursera se enfoca en la facilidad a la hora de usar la pagina. Solo necesitas registrarte y llenar la informacion solicitada. Luego, desde tu perfil, deberas llenar informacion como el titulo y experiencia del proveedor. A continuacion, se podra crear cualquier tipo de servicio y ya podra estar a la espera de nuevos clientes.</p>    
+        </div>
+        <div className="preguntaDiv">
+            <h3>Estoy teniendo problemas tecnicos en la pagina, ¿Con quien puedo hablar?</h3>
+            <p>Cirsera cuenta con múltiples centros de soporte y ayuda al consumidor vía telefónica. Además, cuenta con un chat interactivo desde la página para realizar cancelaciones previas a las 24 horas de hecha la compra sin ningún tipo de costo. Pasadas esas 24 horas, se deberá hablar con el soporte para coordinar las acciones a tomar y se devolverá la totalidad o una parcialidad de la compra.</p>    
         </div>
       </div>  
       <hr />
