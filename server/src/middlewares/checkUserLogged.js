6d702cf2e79edc1a07ea50798af8888e14025f46
Nach0Zar <ignaciozarlenga@hotmail.com>
2023-11-10 18:36:10 -0300

    Removed console.log

--- server/src/middlewares/checkUserLogged.js
+++ server/src/middlewares/checkUserLogged.js
@@ -1,6 +1,7 @@
 import { Error } from "../error/error.js";
 
 export default function checkUserLogged(req, res, next) {
+    console.log(req.cookies)
     if(!req.cookies.email){
         throw new Error('You need to be logged in to perform this action', 'FORBIDDEN')
     }
